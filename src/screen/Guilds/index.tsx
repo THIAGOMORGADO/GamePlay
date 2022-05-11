03ddeef07e3855d5171efd90701320f57f173555
Thiago Morgado <thiago_morgado@hotmail.com>
2022-05-11 04:28:21 -0300

    appointmentsDetaiels

---
+++ src/screen/Guilds/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+/* Estilos */
+import { styles } from './styles'
+
+export function Guilds() {
+  return (
+    <View style={styles.contaienr}>
+      <Text></Text>
+    </View>
+  )
+}
